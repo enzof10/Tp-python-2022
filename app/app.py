cb0382238ad5a6932f7f57f722a60197b7ec2d49
Enzo Frias Borda <enzo_17_05@hotmail.com>
2022-06-14 21:53:47 -0300

    add case if user winner

--- app/app.py
+++ app/app.py
@@ -3,15 +3,17 @@ from curses import window
 from datetime import datetime
 from hashlib import new
 import numbers
+from os import pread
 from unicodedata import name
 from xmlrpc.client import DateTime
+import random
 from click import style
 from flask import Flask, redirect, render_template, json, request, url_for, session
 app = Flask(__name__)
 app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 
 
-@app.route('/' , methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         name = request.form["name"]
@@ -20,41 +22,58 @@ def index():
     return render_template('index.html')
 
 
+def ramdomGame():
+    randoms = []
+    for i in range(5):
+        randoms.append(random.randint(0, 60))
+    return randoms
+
+
 @app.route('/options')
 def options():
-    session['numbers'] = [6, 5, 6, 12, 43 ]
+    randoms = ramdomGame()
+    print(randoms)
+    session['numbers'] = randoms
     return render_template('options.html')
-
 
 
 @app.route('/game', methods=['GET', 'POST'])
 def game():
-    styles  = style('Hello World!', fg='red', bg='blue')
-    clase = ["" ,"" , "", "", "" ]
+    clase = ["", "", "", "", ""]
     userNumbers = request.form
+    print(session['numbers'])
     if request.method == 'POST':
-        session["attempts"] = session["attempts"] + 1 
         userNumbersSave = []
-        for positionUserNumber in  userNumbers.keys():
+        corrects = 0
+        for positionUserNumber in userNumbers.keys():
             userNumbersSave.append(userNumbers[positionUserNumber])
-            for positionRandom in  range(len(session['numbers'])):
-                if int(userNumbers[positionUserNumber]) ==  int(session['numbers'][positionRandom]):
+            for positionRandom in range(len(session['numbers'])):
+                if int(userNumbers[positionUserNumber]) == int(session['numbers'][positionRandom]):
                     if int(positionRandom) == int(positionUserNumber):
                         clase[int(positionUserNumber)] = "green"
+                        corrects = corrects + 1
                         break
                     else:
                         clase[int(positionUserNumber)] = "yellow"
                 elif(clase[int(positionUserNumber)] != "yellow" and clase[int(positionUserNumber)] != "green"):
                     clase[int(positionUserNumber)] = "red"
         session["userNumbers"] = userNumbersSave
-        return render_template('game.html', style = styles, clase = clase)
+        if(corrects == 5):
+            session["isWinner"] = "true"
+            randoms = ramdomGame()
+            session['numbers'] = randoms
+            session["endGame"] = datetime.now()
+            print("ganoo")
+        else:
+            session["isWinner"] = "false"
+            session["attempts"] = session["attempts"] + 1
+
+        return render_template('game.html', clase=clase)
     else:
         session["attempts"] = 0
+        session["isWinner"] = "false"
         session["startGame"] = datetime.now()
-        return render_template('game.html',style = styles, clase = clase)   
-
-def check_numbers():
-    return "OK"
+        return render_template('game.html', clase=clase)
 
 
 def queryString():
@@ -64,14 +83,11 @@ def queryString():
     return "OK"
 
 
-
 def pagina_no_encontrada(error):
     return redirect(url_for('index')), 404
 
 
-
 if __name__ == "app":
-    app.add_url_rule('/queryString', 'queryString', view_func= queryString)
+    app.add_url_rule('/queryString', 'queryString', view_func=queryString)
     app.register_error_handler(404, pagina_no_encontrada)
     app.run(debug=True, port=5000)
-
